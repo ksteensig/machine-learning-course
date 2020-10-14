c1dcde2554917b2809c04329882db8a0925ba7c5
Kasper Steensig Jensen <kasper.steensig@gmail.com>
2020-10-14 13:11:07 +0200

    Finish lecture 7 exercise

--- Lec7/lecture_7_764.py
+++ Lec7/lecture_7_764.py
@@ -3,9 +3,9 @@
 # Members : Bjørn Utrup Dideriksen, Kasper Steensig Jensen, Kristoffer Calundan Derosche
 # Date : 2020/10/14
 # Lecture: 7 Support Vector Machines
-# Dependencies: numpy=1.19.2, scipy=1.5.2, matplotlib=3.3.2, scikit-learn==0.23.2
+# Dependencies: numpy=1.19.2, scipy=1.5.2, matplotlib=3.3.2, scikit-learn=0.23.2
 # Python version: 3.8.2
-# Functionality: Classify the 9D LDA transformed MNIST dataset using an SVM
+# Functionality: Classify MNIST dataset using an SVM
 # ###################################
 
 import numpy as np
@@ -33,18 +33,35 @@ for i in range(10):
     test_label.fill(i)
     test_labels.append(test_label)
 
+# concatenate training data/labels and test data/labels into their own numpy arrays
 train = np.concatenate(np.array(train, dtype=object))
 test = np.concatenate(np.array(test, dtype=object))
 labels = np.concatenate(np.array(labels, dtype=object)).transpose()
 test_labels = np.concatenate(np.array(test_labels, dtype=object)).transpose()
 
-lda = LinearDiscriminantAnalysis(n_components = 9).fit(train, labels)
-lda_train = lda.transform(train)
-lda_test = lda.transform(test)
+# use LDA to reduce the MNIST data to 9 dimensions, can also use PCA with 10 dimensions
 
-svm_mnist = svm.SVC().fit(lda_train, labels)
 
-predictions = svm_mnist.predict(lda_test)
-correct = sum(test_labels == predictions)
+def lda_svm():
+    lda = LinearDiscriminantAnalysis(n_components = 9).fit(train, labels)
+    lda_train = lda.transform(train)
+    lda_test = lda.transform(test)
 
-print('accuracy of the SVM is: ', correct/len(test_labels))
+    svm_mnist = svm.SVC(verbose=True).fit(lda_train, labels)
+
+    predictions = svm_mnist.predict(lda_test)
+    correct = sum(test_labels == predictions)
+    
+    return correct/len(test_labels)
+
+print('9D LDA transformed SVM accuracy: ', lda_svm())
+
+def full_svm():
+    svm_mnist = svm.SVC(verbose=True).fit(train, labels)
+
+    predictions = svm_mnist.predict(test)
+    correct = sum(test_labels == predictions)
+    
+    return correct/len(test_labels)
+
+print('Full SVM accuracy: ', full_svm())
